f1dec5f3f5573423cf2aae18b5f86361b54c49f8
rishabhverma00 <rishabh.verma@transferwise.com>
2025-06-19 11:12:48 +0300

    initial commit

---
+++ src/test/java/com/in/kronos/KronosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.kronos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KronosApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
